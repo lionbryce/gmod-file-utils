3b2f2abd8d6d3d0dfbba4556a908e3962a569941
bryce <lionbryce101@gmail.com>
2023-12-21 12:07:58 -0500

    file.Delete update
    now tries using the timestamp first to reduce looping
    also returns path and original contents
    
    code is generally cleaner as well

--- fileutils.lua
+++ fileutils.lua
@@ -20,23 +20,34 @@ end
 file.UnsafeDelete = file.UnsafeDelete or file.Delete
 
 -- Safely delete. IE: Gmod recycle bin
-function file.Delete(name)
-    local nameWithExtension = name
-    name = string.StripExtension(name)
-    local nameWithPath = "deleted_files/" .. name
-    local fileContent = file.Read(nameWithExtension)
-    if not fileContent then return end
-    local i
+function file.Delete(path)
+    local fileContent = file.Read(path)
+    if not fileContent then return end -- file doesn't exist
+	
+    local originalPath = path
+	local ext = "." .. string.GetExtensionFromFilename(path)
+    path = string.StripExtension(path)	
+    path = "deleted_files/" .. path
+	
 
-    -- If it exists, get new name; name, name_1, name_2, etc
-    while file.Exists(nameWithPath .. (i and ("_" .. i) or "") .. ".txt", "DATA") do
-        if i and i >= 1000 then return end -- Loop limit; Can be removed, but it's staying 
-        i = (i or 0) + 1
-    end
+	if file.Exists(path .. ext, "DATA") then
+		path = path .. "_" .. os.time() -- unlikely that you deleted 2 files in the same second
+		if file.Exists(path .. ext, "DATA") then -- if it does exist, add a number
+			path = path .. "_" -- doing this here to avoid 1 extra concat per loop
 
-    local useName = nameWithPath .. (i and ("_" .. i) or "") .. ".txt" -- I could've kept the variable of the string but it's not that bad
-    file.Write(useName, fileContent) -- Write the backup
-    if not file.Exists(useName, "DATA") then return print("Failed to write backup file while deleting the old file: " .. tostring(nameWithExtension) .. " ; file wasn't deleted") end
+			local i = 1
+			while file.Exists(path .. i .. ext, "DATA") do
+				i = i + 1
+			end
+			path = path .. i
+		end
+	end
+
+	path = path .. ext
+    file.Write(path, fileContent)
+	
+    if not file.Exists(path, "DATA") then return print("Failed to write backup file while deleting the old file: " .. tostring(originalPath) .. " ; file wasn't deleted") end
     -- Only deletes once we know the file exists, I could do a file.Read to compare but that would make sense
-    file.UnsafeDelete(nameWithExtension)
+    file.UnsafeDelete(originalPath)
+	return fileContent, path
 end
